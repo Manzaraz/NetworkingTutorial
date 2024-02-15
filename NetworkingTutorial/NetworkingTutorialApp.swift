ad0986c42fbdc2b5d75d705a35f962b41e2bdf4f
Christian Manzaraz <magnusmanz@gmail.com>
2024-02-15 12:41:46 -0300

    Initial Commit

---
+++ NetworkingTutorial/NetworkingTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingTutorialApp.swift
+//  NetworkingTutorial
+//
+//  Created by Christian Manzaraz on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
